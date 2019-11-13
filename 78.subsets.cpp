58b4583a46302f1fa23ccb20985f1f24479f504e
wangzhen <wangzhenaaa7@gmail.com>
2019-11-13 15:04:12 +0800

    78 subsets problem

--- 78.subsets.cpp
+++ 78.subsets.cpp
@@ -7,9 +7,26 @@
 // @lc code=start
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        
+        vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        int t = 0;
+        for(int i=0; i<n; i++){
+            t+=1<<i;
+        }
+        vector<vector<int>> res;
+        while (t >= 0)
+        {
+            vector<int> tmp_res;
+            for (int i = 0; i < n; i++)
+            {
+                if(t&(1<<i)) tmp_res.push_back(nums[i]);
+            }
+            res.push_back(tmp_res);
+            t--;
+        }
+        return res;
     }
+    
 };
 // @lc code=end
 
